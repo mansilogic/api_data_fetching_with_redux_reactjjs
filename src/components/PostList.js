1ceb452978923d5a1038743727f21ad4cca18fc9
mansilogicwind <mansi.logicwind@gmail.com>
2023-12-23 00:22:27 +0530

    added click event

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
-import {fetchPostsWithRedux } from "../redux/actions";
+import { fetchPostsWithRedux } from "../redux/actions";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPostId: null,
+    };
+  }
+
   componentDidMount() {
     this.props.fetchPostsWithRedux();
   }
+
+  handlePostClick = (postId) => {
+    this.setState({ selectedPostId: postId });
+  };
+
   render() {
+    const { selectedPostId } = this.state;
+    const { posts } = this.props;
+    const selectedPost = posts.find((post) => post.id === selectedPostId);
+
     return (
-      <ul>
-        {this.props.posts &&
-          this.props.posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>; // Adding key for each list item
-          })}
-      </ul>
+      <div>
+        <ul>
+          {posts &&
+            posts.map((post) => (
+              <li key={post.id} onClick={() => this.handlePostClick(post.id)}>
+                <strong>ID:</strong> {post.id} - {post.title}
+              </li>
+            ))}
+        </ul>
+
+        {selectedPost && (
+          <div>
+            <h1>{selectedPost.id}</h1>
+            <h2>{selectedPost.title}</h2>
+            <p>{selectedPost.body}</p>
+          </div>
+        )}
+      </div>
     );
   }
 }
@@ -26,4 +54,4 @@ function mapStateToProps(state) {
 
 let Container = connect(mapStateToProps, { fetchPostsWithRedux })(App);
 
-export default Container; // Exporting the Container component
+export default Container;
